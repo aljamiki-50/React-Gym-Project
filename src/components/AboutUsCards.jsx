d089c8755c6b8bd4c1612f7b4af756e2a53c7845
aljamiki-50 <almazin77@yahoo.com>
2024-01-05 17:00:01 +0000

    adding the happy clients sec full respo

--- src/components/AboutUsCards.jsx
+++ src/components/AboutUsCards.jsx
@@ -2,26 +2,69 @@ import React from "react";
 import Logo from "../img/logo.png";
 import trainer from "../img/trainer.png";
 
-
 function AboutUsCards() {
   return (
-    <div className=" flex  justify-center ring-1/50   ring-black mx-24  gap-x-3  align-middle  text-center   ">
-      <div className="  text-left pl-10 flex flex-col gap-y-10 w-[30%] ">
+    <div
+      className=" flex  justify-center ring-1/50   ring-black mx-24  gap-x-3  align-middle  text-center  
+     s:flex-col-reverse s:mx-0 s:gap-x-0
+    xs:flex-row-reverse  xs:mx-0 xs:gap-x-0
+    md:flex-row-reverse  md:mx-0 md:gap-x-0 md:items-center md:mb-4
+    lg:flex-row-reverse  lg:mx-0 lg:gap-x-0 lg:items-center lg:mb-4
+
+
+      "
+    >
+      <div
+        className="  text-left pl-10 flex flex-col gap-y-10 w-[30%]
+       s:w-[100%] s:items-center s:gap-y-5
+       xs:w-[100%] xs:items-center xs:gap-y-5 xs:flex-col-reverse
+       md:w-[100%] md:items-center md:gap-y-0  md:flex-col-reverse   md:grid   md:grid-cols-1  
+       lg:w-[100%] lg:items-center lg:gap-y-0  lg:flex-col-reverse   lg:grid   lg:grid-cols-1  
+
+
+
+       "
+      >
         {/* <div className="   "> */}
         <img
           src={Logo}
-          className="  bg-black  w-20 h-20 object-contain  rounded-[1000%]  mt-10 ring-1 "
+          className="  bg-black  w-20 h-20 object-contain  rounded-[1000%]  mt-10 ring-1 
+          md:order-3
+          lg:order-3
+
+           "
           alt=""
         />
 
         {/* </div> */}
-        <p className=" w-[80%] text-[#6D6D6D] ">
+        <p
+          className=" w-[80%] text-[#6D6D6D]
+        s:w-full s:text-lg s:font-bold s:tracking-wide
+        xs:w-full xs:text-lg xs:font-bold xs:tracking-wide
+        md:order-2
+        lg:order-2
+
+         "
+        >
           ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
           veniam, quis no.
         </p>
-        <div>
-          <h1 className=" text-black poppins font-bold text-2xl">
+        <div
+          className="
+        gx:mb-10
+         s:mb-10    s:grid grid-cols-1 s:self-start
+         xs:mb-10    xs:grid grid-colxs-1 xs:self-start
+         md:mb-10    md:grid grid-colxs-1 md:self-start md:order-1
+         lg:mb-10    lg:grid grid-colxs-1 lg:self-start lg:order-1
+
+
+         
+
+
+        "
+        >
+          <h1 className=" text-black poppins font-bold text-2xl  ">
             Sara Thompson
           </h1>
           <p className=" poppins font-bold text-xl text-[#6D6D6D]">
@@ -29,8 +72,8 @@ function AboutUsCards() {
           </p>
         </div>
       </div>
-      <div>
-        <img src={trainer} alt="" />
+      <div className=" s:w-full xs:w-full md:w-full lg:w-full">
+        <img src={trainer} className=" s:w-full xs:w-full  md:w-full  lg:w-full" alt="" />
       </div>
     </div>
   );
--- src/components/AboutUsLight.jsx
+++ src/components/AboutUsLight.jsx
@@ -9,17 +9,29 @@ import AboutSwiper from "./AboutSwiper";
 
 function AboutUsLight() {
   return (
-    <div className="">
+    <div
+      className="container w-screen  min-h-screen my-24
+      lg:mb-96  lg:mt-[4%] 
+    xl:mb-96  xl:mt-[4%] 
+        "
+    >
       <div className=" absolute top-0 left-0 bg-slate-200 w-screen h-full -z-10"></div>
+      {/* <div className=" absolute top-0 left-0 bg-slate-200 w-screen h-full -z-10"></div> */}
 
       <div>
         <div>
           <ExploreText
-            p1Style=" roboto text-lg font-bold pb-4 text-[#426666] w-[50%] grid  mx-auto "
-            h1Style="text-4xl mt-10  text-black poppins font-bold    tracking-wide"
+            p1Style=" roboto text-lg font-bold pb-4 text-[#426666] w-[50%] grid  mx-auto
+             s:w-full s:text-left s:text-wrap s:font-semibold
+              xs:text-wrap xs:font-semibold
+             "
+            h1Style="text-4xl mt-10  text-black poppins font-bold    tracking-wide
+            s:w-full s:text-left s:text-wrap 
+            xs:w-full xs:text-wrap 
+            "
             text1="ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             text2
-            title="Happy Clients"
+            title="Happy Clients :"
           />
         </div>
         {/* <div>
@@ -28,7 +40,11 @@ function AboutUsLight() {
 
             
         </div> */}
-        <div className="    text-black">
+        <div className="    text-black
+
+        
+       
+        ">
           <h1>
             <AboutSwiper />
           </h1>
